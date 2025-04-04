f6a967ebd7d5a79650874c2cd871e0b7b113d5e3
NeoD64 <n.deward@student.maastrichtuniversity.nl>
2025-04-04 14:23:08 +0200

    change in the implementation of getRecords

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -54,7 +54,7 @@ public class Patient {
     public List<PatientRecord> getRecords(long startTime, long endTime) {
         // TODO Implement and test this method
         List <PatientRecord> timeRecords=new ArrayList<>();
-        for(int i=0;i<=patientRecords.size();i++) {
+        for(int i=0;i<patientRecords.size();i++) {
             if(patientRecords.get(i).getTimestamp()>=startTime &&patientRecords.get(i).getTimestamp()<=endTime)
                 timeRecords.add(patientRecords.get(i));
         }
